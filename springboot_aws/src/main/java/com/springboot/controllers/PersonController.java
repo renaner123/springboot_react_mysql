bb16da91f4991b46f5a10deade1872beeea2404a
renaner123 <renaner123@gmail.com>
2024-01-27 20:54:35 -0300

    feat: add simple CRUD to Person

--- springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
+++ springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
@@ -3,9 +3,11 @@ package com.springboot.controllers;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +20,45 @@ import com.springboot.services.PersonService;
 public class PersonController {
 
 	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = LoggerFactory.getLogger(PersonService.class);
 
 	@Autowired
 	private PersonService personService;
 
-	@RequestMapping(value = "/person/{id}", method = RequestMethod.GET, produces = "application/json")
-	public Person personFindById(@PathVariable("id") String id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+	public Person findById(@PathVariable("id") String id) {
+		logger.info("Finding one person!");
 		return personService.findById(id);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public List<Person> personFindAll() {
+	public List<Person> findAll() {
+		logger.info("Finding all persons!");
 		return personService.findall();
 	}
+
+	@RequestMapping(method = RequestMethod.POST, 
+			consumes = "application/json",
+			produces = "application/json")
+	public Person create(@RequestBody Person person) {
+		logger.info("Creating a person!");
+		return personService.create(person);
+	}
+
+	@RequestMapping(value = "/{id}",
+			method = RequestMethod.PUT, 
+			consumes = "application/json",
+			produces = "application/json")
+	public Person update(@RequestBody Person person) {
+		logger.info("Updating a person!");
+		return personService.update(person);
+	}
+
+	@RequestMapping(value = "/{id}",
+	method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") String id){
+		logger.info("Deleting a person!");
+		personService.delete(id);
+	}
+
 }
--- springboot_aws/src/main/java/com/springboot/services/PersonService.java
+++ springboot_aws/src/main/java/com/springboot/services/PersonService.java
@@ -47,6 +47,20 @@ public class PersonService {
         return persons;
     }
 
+    public Person create(Person person){
+        logger.info("Creating a person!");
+        return person;
+    }
+
+    public Person update(Person person){
+        logger.info("Updating a person!");
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("Deleting a person!");
+    }
+
     private Person mockPerson(int i) {
         Person person = new Person();
         person.setId(counter.incrementAndGet());
