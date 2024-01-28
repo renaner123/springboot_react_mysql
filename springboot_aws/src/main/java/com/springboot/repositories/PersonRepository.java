a5093c0264c0c98c236cfdb1171117f55ca65a19
renaner123 <renaner123@gmail.com>
2024-01-27 23:20:55 -0300

    refactor: remove spring warnings

--- springboot_aws/src/main/java/com/springboot/repositories/PersonRepository.java
+++ springboot_aws/src/main/java/com/springboot/repositories/PersonRepository.java
@@ -1,9 +1,7 @@
 package com.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.springboot.models.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
