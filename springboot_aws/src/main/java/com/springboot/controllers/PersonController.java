69b96051abe011bc580087c504706895d6e32aac
renaner123 <renaner123@gmail.com>
2024-01-27 22:47:03 -0300

    refactor: replace RequestMapping

--- springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
+++ springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
@@ -1,15 +1,19 @@
 package com.springboot.controllers;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.models.Person;
@@ -19,46 +23,44 @@ import com.springboot.services.PersonService;
 @RequestMapping("/person")
 public class PersonController {
 
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = LoggerFactory.getLogger(PersonService.class);
 
 	@Autowired
 	private PersonService personService;
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") Long id) {
 		logger.info("Finding one person!");
 		return personService.findById(id);
 	}
 
-	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	// INFO MediaType é necessário para o Swagger funcionar corretamente
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll() {
 		logger.info("Finding all persons!");
 		return personService.findall();
 	}
 
-	@RequestMapping(method = RequestMethod.POST, 
-			consumes = "application/json",
-			produces = "application/json")
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person create(@RequestBody Person person) {
 		logger.info("Creating a person!");
 		return personService.create(person);
 	}
 
-	@RequestMapping(value = "/{id}",
-			method = RequestMethod.PUT, 
-			consumes = "application/json",
-			produces = "application/json")
+	@PutMapping(value = "/{id}",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person update(@RequestBody Person person) {
 		logger.info("Updating a person!");
 		return personService.update(person);
 	}
 
-	@RequestMapping(value = "/{id}",
-	method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") Long id){
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
 		logger.info("Deleting a person!");
 		personService.delete(id);
+		return ResponseEntity.noContent().build();	
 	}
 
 }
