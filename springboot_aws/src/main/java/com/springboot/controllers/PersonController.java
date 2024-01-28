1ea2f19299249357943285d8111abb9709183a82
renaner123 <renaner123@gmail.com>
2024-01-27 23:53:57 -0300

    feat: add dozer mapper to convert Value Objects to Person Object

--- springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
+++ springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
@@ -16,50 +16,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.models.Person;
+import com.springboot.data.vo.v1.PersonVO;
 import com.springboot.services.PersonService;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/PersonVO")
 public class PersonController {
 
 	private Logger logger = LoggerFactory.getLogger(PersonService.class);
 
 	@Autowired
-	private PersonService personService;
+	private PersonService PersonVOService;
 
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable("id") Long id) {
-		logger.info("Finding one person!");
-		return personService.findById(id);
+	public PersonVO findById(@PathVariable("id") Long id) {
+		logger.info("Finding one PersonVO!");
+		return PersonService.findById(id);
 	}
 
 	// INFO MediaType é necessário para o Swagger funcionar corretamente
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
-		logger.info("Finding all persons!");
-		return personService.findall();
+	public List<PersonVO> findAll() {
+		logger.info("Finding all PersonVOs!");
+		return PersonService.findall();
 	}
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
-		logger.info("Creating a person!");
-		return personService.create(person);
+	public PersonVO create(@RequestBody PersonVO PersonVO) {
+		logger.info("Creating a PersonVO!");
+		return PersonService.create(PersonVO);
 	}
 
 	@PutMapping(value = "/{id}",
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
-		logger.info("Updating a person!");
-		return personService.update(person);
+	public PersonVO update(@RequestBody PersonVO PersonVO) {
+		logger.info("Updating a PersonVO!");
+		return PersonVOService.update(PersonVO);
 	}
 
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
-		logger.info("Deleting a person!");
-		personService.delete(id);
+		logger.info("Deleting a PersonVO!");
+		PersonVOService.delete(id);
 		return ResponseEntity.noContent().build();	
 	}
 
