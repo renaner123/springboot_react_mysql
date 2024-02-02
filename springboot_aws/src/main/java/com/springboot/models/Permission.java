5ab23d6cf33a21d2bba5fcf1b6f99ecc2dab58b3
Renan Rodolfo da Silva <renaner123@gmail.com>
2024-02-02 11:24:12 -0300

    feat: Integrate Spring Security, JWT, and Entity Mapping
    This commit implements Spring Security for enhanced application security. It also incorporates JWT (JSON Web Token) for authentication purposes. Additionally, entity mapping functionality has been added to ensure proper data handling within the application.

---
+++ springboot_aws/src/main/java/com/springboot/models/Permission.java
@@ -0,0 +1,46 @@
+package com.springboot.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    public Permission() {
+    }
+    
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
