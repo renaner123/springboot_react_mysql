3d3e1e10df2753e3d7fcb9b7dd155827ac396baf
renaner123 <renaner123@gmail.com>
2024-02-05 20:54:21 -0300

    refactor: fix: Resolve Spring Boot deprecation warning

--- springboot_aws/src/main/java/com/springboot/configs/SecurityConfig.java
+++ springboot_aws/src/main/java/com/springboot/configs/SecurityConfig.java
@@ -10,18 +10,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.springboot.security.jwt.JwtConfigurer;
+import com.springboot.security.jwt.JwtTokenFilter;
 import com.springboot.security.jwt.JwtTokenProvider;
-
-
 
 @EnableWebSecurity
 @Configuration
@@ -29,49 +27,36 @@ public class SecurityConfig {
 
 	@Autowired
 	private JwtTokenProvider tokenProvider;
-	
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		Map<String, PasswordEncoder> encoders = new HashMap<>();
-				
-		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000,
+				SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
 		encoders.put("pbkdf2", pbkdf2Encoder);
 		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
 		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
 		return passwordEncoder;
 	}
-	
-    @Bean
-    AuthenticationManager authenticationManagerBean(
-    		AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic().disable()
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-            		session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(
-                    authorizeHttpRequests -> authorizeHttpRequests
-                        .requestMatchers(
-							"/auth/signin",
-							"/auth/refresh/**",
-                    		"/swagger-ui/**",
-                    		"/v3/api-docs/**"
-                		).permitAll()
-                        .requestMatchers("/api/**").authenticated()
-                        .requestMatchers("/users").denyAll()
-                )
-                .cors()
-                .and()
-                .apply(new JwtConfigurer(tokenProvider))
-                .and()
-                .build();
- 
-    }
+	@Bean
+	AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+		return http.httpBasic(basic -> basic.disable()).csrf(csrf -> csrf.disable())
+				.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+						.requestMatchers("/auth/signin", "/auth/refresh/**", "/swagger-ui/**", "/v3/api-docs/**")
+						.permitAll().requestMatchers("/api/**").authenticated().requestMatchers("/users").denyAll())
+				.cors(cors -> {
+				}).build();
+
+	}
 }
-
--- springboot_aws/src/main/java/com/springboot/data/vo/security/TokenVO.java
+++ springboot_aws/src/main/java/com/springboot/data/vo/security/TokenVO.java
@@ -11,17 +11,17 @@ public class TokenVO implements Serializable{
     private Boolean authenticated;
     private Date created;
     private Date expiration;
-    private String acessToken;
+    private String accessToken;
     private String refreshToken;
 
  
-    public TokenVO(String username, Boolean authenticated, Date created, Date expiration, String acessToken,
+    public TokenVO(String username, Boolean authenticated, Date created, Date expiration, String accessToken,
             String refreshToken) {
         this.username = username;
         this.authenticated = authenticated;
         this.created = created;
         this.expiration = expiration;
-        this.acessToken = acessToken;
+        this.accessToken = accessToken;
         this.refreshToken = refreshToken;
     }
 
@@ -64,12 +64,12 @@ public class TokenVO implements Serializable{
         this.expiration = expiration;
     }
 
-    public String getAcessToken() {
-        return acessToken;
+    public String getaccessToken() {
+        return accessToken;
     }
 
-    public void setAcessToken(String acessToken) {
-        this.acessToken = acessToken;
+    public void setaccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 
     public String getRefreshToken() {
@@ -88,7 +88,7 @@ public class TokenVO implements Serializable{
         result = prime * result + ((authenticated == null) ? 0 : authenticated.hashCode());
         result = prime * result + ((created == null) ? 0 : created.hashCode());
         result = prime * result + ((expiration == null) ? 0 : expiration.hashCode());
-        result = prime * result + ((acessToken == null) ? 0 : acessToken.hashCode());
+        result = prime * result + ((accessToken == null) ? 0 : accessToken.hashCode());
         result = prime * result + ((refreshToken == null) ? 0 : refreshToken.hashCode());
         return result;
     }
@@ -122,10 +122,10 @@ public class TokenVO implements Serializable{
                 return false;
         } else if (!expiration.equals(other.expiration))
             return false;
-        if (acessToken == null) {
-            if (other.acessToken != null)
+        if (accessToken == null) {
+            if (other.accessToken != null)
                 return false;
-        } else if (!acessToken.equals(other.acessToken))
+        } else if (!accessToken.equals(other.accessToken))
             return false;
         if (refreshToken == null) {
             if (other.refreshToken != null)
