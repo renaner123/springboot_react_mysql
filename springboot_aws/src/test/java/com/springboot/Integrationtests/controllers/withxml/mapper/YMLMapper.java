d831775cc54430127d5f48d3cf38b7011c233a2b
Renan Rodolfo da Silva <renaner123@gmail.com>
2024-02-06 16:19:55 -0300

    test: implement AuthController Yaml test

---
+++ springboot_aws/src/test/java/com/springboot/Integrationtests/controllers/withxml/mapper/YMLMapper.java
@@ -0,0 +1,47 @@
+package com.springboot.Integrationtests.controllers.withxml.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.mapper.ObjectMapperDeserializationContext;
+import io.restassured.mapper.ObjectMapperSerializationContext;
+
+public class YMLMapper implements ObjectMapper {
+
+    private com.fasterxml.jackson.databind.ObjectMapper objectMapper;
+    protected TypeFactory typeFactory;
+
+    public YMLMapper(TypeFactory typeFactor) {
+        objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        typeFactory = TypeFactory.defaultInstance();
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public Object deserialize(ObjectMapperDeserializationContext context) {
+        // FIXME melhorar, está assim apenas para funcionar
+        try {
+            String dataToDeserialize = context.getDataToDeserialize().asString();
+            Class type = (Class) context.getType();
+            return objectMapper.readValue(dataToDeserialize, typeFactory.constructType(type));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return context;
+    }
+
+    @Override
+    public Object serialize(ObjectMapperSerializationContext context) {
+        // FIXME melhorar, está assim apenas para funcionar
+        try {
+            return objectMapper.writeValueAsString(context.getObjectToSerialize());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return context;
+    }
+
+}
---
+++ springboot_aws/src/test/java/com/springboot/Integrationtests/controllers/withyaml/AuthControllerYamlTest.java
@@ -0,0 +1,72 @@
+package com.springboot.Integrationtests.controllers.withyaml;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.springboot.Integrationtests.VO.AccountCredentialsVO;
+import com.springboot.Integrationtests.VO.TokenVO;
+import com.springboot.Integrationtests.testcontainers.AbstractIntegrationTest;
+import com.springboot.configs.TestConfigs;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+public class AuthControllerYamlTest extends AbstractIntegrationTest{
+
+    private static TokenVO tokenVO;
+
+	@Test
+	@Order(1)
+	public void testSignin() throws JsonMappingException, JsonProcessingException{
+		AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
+        
+		tokenVO = 
+				given()
+				    .basePath("/auth/signin")
+				    	.port(TestConfigs.SERVER_PORT)
+				    	.contentType(TestConfigs.CONTENT_TYPE_XML)
+					.body(user)
+						.when()
+					.post()
+						.then()
+							.statusCode(200)
+								.extract()
+								.body()
+									.as(TokenVO.class);
+
+        assertNotNull(tokenVO.getaccessToken());
+        assertNotNull(tokenVO.getRefreshToken());
+        //poderia verificar se o token é válido, mas não é o objetivo desse teste.								
+	}
+
+	@Test
+	@Order(2)
+	public void testRefresh() throws JsonMappingException, JsonProcessingException{
+        
+		var newTokenVO = 
+				given()
+				    .basePath("/auth/refresh")
+				    	.port(TestConfigs.SERVER_PORT)
+				    	.contentType(TestConfigs.CONTENT_TYPE_JSON)
+					.pathParam("username", tokenVO.getUsername())
+                    .header(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO.getRefreshToken())
+					.when()
+					    .put("{username}")
+					.then()
+						.statusCode(200)
+					.extract()
+						.body()
+							.as(TokenVO.class);
+
+        assertNotNull(newTokenVO.getaccessToken());
+        assertNotNull(newTokenVO.getRefreshToken());
+        //poderia verificar se o token é válido, mas não é o objetivo desse teste.								
+	}    
+}
