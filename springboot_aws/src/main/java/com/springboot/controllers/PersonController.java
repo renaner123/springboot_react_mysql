bf00f5d51dd7b3f9a36054a4a77a54e1a72463d5
renaner123 <renaner123@gmail.com>
2024-02-14 14:58:13 -0300

    feat: add hateoas to pageable Person

--- springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
+++ springboot_aws/src/main/java/com/springboot/controllers/PersonController.java
@@ -1,11 +1,12 @@
 package com.springboot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -55,7 +56,7 @@ public class PersonController {
 			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
 		}
 	)
-	public ResponseEntity<Page<PersonVO>> findAll(
+	public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page, 
 			@RequestParam(value = "size", defaultValue = "12") Integer size, 
 			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
