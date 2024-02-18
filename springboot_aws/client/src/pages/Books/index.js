3332e3fb4962edb24b4019f5d4883f053d8cdde0
renaner123 <renaner123@gmail.com>
2024-02-18 20:36:22 -0300

    feat: structure for book list page

---
+++ springboot_aws/client/src/pages/Books/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiEdit, FiPower, FiTrash2 } from 'react-icons/fi';
+import './styles.css';
+
+import logoImage from '../../asserts/logo.svg';
+
+export default function Books() { 
+    return (
+
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Application logo" />
+                <span>Welcome, <strong>Renan</strong> </span>
+                <Link className="button" to="books/new">Add new Book</Link>
+                <button type="button">
+                    <FiPower size={18} color="#251FC5" />
+                </button>
+            </header>
+
+            <h1>Registered Books</h1>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>React Native</p>
+
+                    <strong>Author:</strong>
+                    <p>Renan</p>
+
+                    <strong>Price:</strong>
+                    <p>$ 120.00</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+                    
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>                
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>React Native</p>
+
+                    <strong>Author:</strong>
+                    <p>Renan</p>
+
+                    <strong>Price:</strong>
+                    <p>$ 120.00</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+                    
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>                
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>React Native</p>
+
+                    <strong>Author:</strong>
+                    <p>Renan</p>
+
+                    <strong>Price:</strong>
+                    <p>$ 120.00</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" />
+                    </button>
+                    
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>                
+                </li>
+            
+            </ul>
+        </div>
+
+    );
+}
--- springboot_aws/client/src/routes.js
+++ springboot_aws/client/src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from './pages/Login';
+import Books from './pages/Books';
 
 export default function RoutesApp(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login/>} />
+                <Route path="/books" element={<Books/>} />
             </Routes>
         </BrowserRouter>
     );
