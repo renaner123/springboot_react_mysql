9ae0a81160f6fbe39db9faa568b52242b5eeea43
renaner123 <renaner123@gmail.com>
2024-02-18 21:20:05 -0300

    feat: integrate login page to server API

--- springboot_aws/client/src/pages/Login/index.js
+++ springboot_aws/client/src/pages/Login/index.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
+
 
 import logoImage from '../../asserts/logo.svg';
 import padlock from '../../asserts/padlock.png';
+import api from '../../services/api';
 
 export default function Login() { 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        const data = {
+            username,
+            password,
+        };
+
+        try{
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.accessToken );
+
+            navigate('/books');
+
+        } catch(err){
+            alert('Error, try again.');
+        }
+    };
+
     return (
         <div className="login-container">  
             <section className="form">
                 <img src={logoImage} alt="Be The Hero"/>
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Access your account</h1>
-                    <input placeholder="Username"/>
-                    <input type="password" placeholder="Password"/>
-                    
+                    <input 
+                        placeholder="Username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}    
+                    />
+                    <input type="password" 
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />                    
                     
                     <button className="button" type="submit">Login</button>
                 </form>
