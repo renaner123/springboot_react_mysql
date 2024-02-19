f464a955432f4bbaa52655fe13b4aa4b672bc18e
renaner123 <renaner123@gmail.com>
2024-02-18 23:07:32 -0300

    feat: integrate Book add page with API

--- springboot_aws/client/src/pages/NewBook/index.js
+++ springboot_aws/client/src/pages/NewBook/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FiArrowLeft, FiEdit, FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 
+import api from '../../services/api';
+
 import logoImage from '../../asserts/logo.svg';
 
-
 export default function NewBook() {
+    const [id, setid] = useState('');
+    const [author, setAuthor] = useState('');
+    const [launch_date, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    const navigate = useNavigate();
+
+    async function createNewBook(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            author,
+            launch_date, 
+            price
+        };
+
+        const username = localStorage.getItem('username');
+        const accessToken = localStorage.getItem('accessToken');
+
+        try {
+            await api.post('api/book/v1', data, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            
+            });
+            navigate('/books');
+        } catch (error) {
+            console.error('Error:', error.message); // Log the error message
+          }
+    };
+    
 
     return(
         <div className="new-book-container">
@@ -20,11 +55,27 @@ export default function NewBook() {
                         Home
                     </Link>
                 </section>
-                <form action="">
-                    <input placeholder="Title" />
-                    <input placeholder="Author" />
-                    <input placeholder="date" />
-                    <input placeholder="Price" />
+                <form onSubmit={createNewBook}>
+                    <input 
+                        placeholder="Title" 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}                    
+                    />
+                    <input 
+                        placeholder="Author" 
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}                    
+                    />
+                    <input 
+                        type="date"
+                        value={launch_date}
+                        onChange={e => setLaunchDate(e.target.value)}                    
+                    />
+                    <input 
+                        placeholder="Price" 
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}                    
+                    />
 
                     <button className="button" type="submit">Add</button>
                 </form>
